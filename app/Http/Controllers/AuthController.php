b9cb96e4fe3d3d6cbe52ab4ff7f0e0987843e460
Kchanit <chanitaphon.l@ku.th>
2023-09-03 19:26:44 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'password' => 'required',
+        ]);
+
+        $user = new User();
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->password = bcrypt($request->get('password')); // Hash the password
+        // 'password' => bcrypt($data['password']), // Hash the password
+        $user->save();
+
+        return ['message' => 'User created successfully', 'success' => true];
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // Logic to retrieve a list of users
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique',
+            'password' => 'required',
+        ]);
+
+        $user = new User();
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->password = $request->get('password'); // Hash the password
+        // 'password' => bcrypt($data['password']), // Hash the password
+
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+
+    public function show($id)
+    {
+        // Logic to retrieve a specific user by ID
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logic to update a user by ID
+    }
+
+    public function destroy($id)
+    {
+        // Logic to delete a user by ID
+    }
+}
