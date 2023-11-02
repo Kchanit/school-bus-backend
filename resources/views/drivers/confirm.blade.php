2279e306526dd3e0cef91776e1246362e8200a1a
thyms-c <thanyamas.c@ku.th>
2023-11-02 22:45:51 +0700

    feat: edit drivr fix

--- resources/views/drivers/confirm.blade.php
+++ resources/views/drivers/confirm.blade.php
@@ -2,10 +2,10 @@
 @section('content')
     <section class="flex container justify-center mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div class="flex flex-col justify-center w-1/2 bg-white rounded-lg shadow-xl p-4">
-            <h1 class="text-2xl font-bold text-center">
-                Create Complete !
+            <h1 class="text-2xl font-bold text-center mt-6">
+                Create Successfully!
             </h1>
-            <div class="flow-root m-8">
+            <div class="flow-root m-8 mt-9">
                 <dl class="-my-3 divide-y divide-gray-100 text-sm">
 
                     <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
@@ -22,20 +22,20 @@
                         <dd class="text-gray-700 sm:col-span-2" id="modalEmail">{{ $driver->email }}</dd>
                     </div>
                 </dl>
-                <div class="gap-6">
-                    <h1 class="text-2xl font-bold text-center">
+                <div class="flex flex-col gap-y-2 mt-8">
+                    <h1 class="text-2xl text-gray-800 font-bold text-center">
                         Password
                     </h1>
-                    <h1 class="text-md text-center">
-                        It will only show once, Please save before confirming.
+                    <h1 class="text-md text-gray-800 text-center">
+                        It will only show once, Please save before confirming or refreshing.
                     </h1>
-                    <h1 class="text-2xl text-red-600 font-bold text-center">
+                    <h1 class="text-2xl mt-4 text-red-600 font-bold text-center">
                         {{ $password }}
                     </h1>
                 </div>
-                <a href="{{ route('drivers.list') }}">
+                <a href="{{ route('drivers.list') }}" class="flex justify-center">
                     <button
-                        class="block text-white mt-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        class="block text-white w-full mt-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         type="button">
                         Confirm
                     </button>
--- resources/views/drivers/list.blade.php
+++ resources/views/drivers/list.blade.php
@@ -114,32 +114,39 @@
                                                 <span class="sr-only">Close modal</span>
                                             </button>
                                             <div class="p-6 text-center">
-                                                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
-                                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 20 20">
-                                                    <path stroke="currentColor" stroke-linecap="round"
-                                                        stroke-linejoin="round" stroke-width="2"
-                                                        d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                                </svg>
+                                                <button data-modal-hide="popup-modal-{{ $driver->id }}" type="button">
+                                                    <svg class="mx-auto
+                                                    mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 20 20">
+                                                        <path stroke="currentColor" stroke-linecap="round"
+                                                            stroke-linejoin="round" stroke-width="2"
+                                                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                    </svg>
+                                                </button>
+
                                                 <h3 class="text-lg font-normal text-gray-500">Are
                                                     you sure you want to delete {{ $driver->getFullName() }} ?</h3>
-                                                <p class="mb-8 text-md font-normal text-gray-500"> when
-                                                    you delete, Driver's route will also disappear. </p>
+                                                <p class="mb-8 mt-1 text-sm font-normal text-gray-500"> When
+                                                    you delete, driver will remove from route. </p>
                                                 {{-- <h1>{{ $driver->id }} {{ $driver->getFullName() }} </h1> --}}
-                                                <form action="{{ route('drivers.destroy', ['driver' => $driver]) }}"
-                                                    method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
+                                                <div class="flex justify-center">
+                                                    <form action="{{ route('drivers.destroy', ['driver' => $driver]) }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
 
+                                                        <button data-modal-hide="popup-modal-{{ $driver->id }}"
+                                                            type="submit"
+                                                            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                                            Yes, I'm sure
+                                                        </button>
+                                                    </form>
                                                     <button data-modal-hide="popup-modal-{{ $driver->id }}"
-                                                        type="submit"
-                                                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                                        Yes, I'm sure
-                                                    </button>
-                                                </form>
-                                                <button data-modal-hide="popup-modal-{{ $driver->id }}" type="button"
-                                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-                                                    cancel</button>
+                                                        type="button"
+                                                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
+                                                        cancel</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
