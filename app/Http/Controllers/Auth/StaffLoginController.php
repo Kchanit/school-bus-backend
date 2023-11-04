326227a660c58c8a92a4c33dc9e3000bccaf9af6
Kchanit <chanitaphon.l@ku.th>
2023-11-04 09:52:29 +0700

    fix: staff login

--- app/Http/Controllers/Auth/StaffLoginController.php
+++ app/Http/Controllers/Auth/StaffLoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Driver;
+use App\Models\Route;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StaffLoginController extends Controller
@@ -22,8 +25,21 @@ class StaffLoginController extends Controller
 
         if (auth()->guard('staff')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
             $staff = auth()->guard('staff')->user();
-            return view('staff.index', ['staff' => $staff]);
-            // return view('home', ['staff' => $staff]);
+
+            $drivers = Driver::with('route')->get();
+            $students = Student::has('address')->get();
+            $routes = Route::get();
+            $avb_drivers = Driver::doesntHave('route')->get();
+            $route_stds = Student::has('address')->doesntHave('route')->get();
+            $all_students = Student::get();
+            return view('staff.index', [
+                'drivers' => $drivers,
+                'students' => $students,
+                'routes' => $routes,
+                'avb_drivers' => $avb_drivers,
+                'route_stds' => $route_stds,
+                'all_students' => $all_students,
+            ]);
         }
 
         return redirect()->back()->withInput($request->only('email', 'remember'))->withErrors([
